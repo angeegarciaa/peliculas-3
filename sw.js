175b5b091d3737e9871edfcd911457addd337a6c
angeegarciaa <alplato2008@gmail.com>
2026-09-18 16:12:58 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+
+// Delega el 100% de las peticiones a la red sin guardar nada localmente
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+});
